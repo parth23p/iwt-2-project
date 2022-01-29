93da2ac7c0f62a35bc29383e5678ba3e4577deab
parth23p <64831233+parth23p@users.noreply.github.com>
2022-01-29 16:37:22 +0530

    port changed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(function (err, req, res, next) {
   next(err);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in port ${PORT}`));
 
 process.on("unhandledRejection", (err, promise) => {
